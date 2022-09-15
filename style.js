4632aaa72db69191f8c3644f267319475257845f
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-15 13:15:37 +0100

    addedtime and alarm

---
+++ style.js
@@ -0,0 +1,45 @@
+const timer = () =>{
+    const date = new Date()
+
+    const hour = (date.getHours())
+    const minutes = (date.getMinutes())
+    const seconds = (date.getSeconds())
+    show.innerHTML= `${hour} : ${minutes} : ${seconds} `
+    show.style.fontSize = "50px"
+    show.style.color = "black"
+    show.style.fontWeight = "bold"
+}
+setInterval(timer, 1000);
+
+let song = new Audio("good.wav")
+const alarm =() => {
+    let myHour = setHour.value
+    let myMinute = setMinute.value
+    d = new Date()
+    if(myHour == d.getHours() && myMinute == d.getMinutes()){
+            song.play()
+    }
+    setTimeout(alarm,1000)
+}
+
+const stopAlarm = () =>{
+    let stp = prompt("DO YOU HAVE ANY ACTIVITY FOR THE DAY?")
+    if(stp = "Yes"){
+        alert("Wake up")
+    }
+    else if(stp = "No"){
+       alert("Rest")
+    }
+}
+
+
+
+// function timer(){
+//     const date =new Date();
+
+//     const hour =(date.getHours());
+//     const minutes =(date.getMinutes());
+//     const seconds =(date.getSeconds())
+//     show.innerHTML= `${hour} : ${minutes} : ${seconds}`
+//     setInterval(timer, 1000);
+// }
