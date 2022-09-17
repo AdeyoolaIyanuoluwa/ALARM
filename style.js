5290d58c8f5255d9e9ea5b451480ee569460084c
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-17 12:25:41 +0100

    responsiveness

--- style.js
+++ style.js
@@ -4,10 +4,7 @@ const timer = () =>{
     const hour = (date.getHours())
     const minutes = (date.getMinutes())
     const seconds = (date.getSeconds())
-    show.innerHTML= `${hour} : ${minutes} : ${seconds} <img src="clock-removebg-preview.png" height="70" alt="">`
-    // show.style.fontSize = "32px"
-    // show.style.color = "black"
-    // show.style.fontWeight = "bold"
+    show.innerHTML= `${hour} : ${minutes} : ${seconds} <img class="img" src="clock-removebg-preview.png" height="70" alt="">`
 }
 setInterval(timer, 1000);
 
